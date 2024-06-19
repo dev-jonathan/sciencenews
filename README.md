8f237a82b9b5c7a87ffa1a783127030dfbc69ee8
Jonathan Borges <88299896+dev-jonathan@users.noreply.github.com>
2024-06-19 17:11:25 +0000

    adicionar script de lintcom prettier

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Clone do site tabnews baseado no curso.dev do @deschamps
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-function Home(){
-    return <h1>hello word 2</h1>
+function Home() {
+  return <h1>hello word 2</h1>;
 }
 
 export default Home;

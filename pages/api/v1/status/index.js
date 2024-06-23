1c16d74554100b6e972e299acc0f0df960a5a11a
Jonathan Borges <88299896+dev-jonathan@users.noreply.github.com>
2024-06-23 22:45:51 +0000

    adicionado data no /status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,11 +1,8 @@
-import database from "infra/database.js";
-
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1;");
-  console.log(result.rows);
+  const updateAt = new Date().toISOString();
 
   response.status(200).json({
-    chave: "valor",
+    update_at: updateAt,
   });
 }
 

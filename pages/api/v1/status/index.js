fe4981d35b467e8da9bb361dcdae8889044a670b
Jonathan Borges <88299896+dev-jonathan@users.noreply.github.com>
2024-06-24 12:01:59 +0000

    adicionado data e hora no /status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,6 +1,8 @@
+import database from "/infra/database";
+
 async function status(request, response) {
   const updateAt = new Date().toISOString();
-
+  console.log(updateAt);
   response.status(200).json({
     update_at: updateAt,
   });
